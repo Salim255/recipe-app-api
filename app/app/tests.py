a2291b89b300de32f093eaf02fc43352129e7efb
Salim <salim.hassan.welleat@gmail.com>
2024-03-23 01:23:05 +0100

    fix: lint error

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,9 +7,11 @@ from django.test import SimpleTestCase
 from app import calc
 
 class CalcTests(SimpleTestCase):
+
     """Test the calc module."""
 
     def test_add_numbers(self):
+        
         """Test adding numbers together"""
 
         result = calc.add(3, 4)
