6be5e98bb7193c47a8307cfcf714a74de22307aa
Salim <salim.hassan.welleat@gmail.com>
2024-03-23 10:03:51 +0100

    test: first TDD test in Django

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,19 +1,18 @@
 """
 Sample test
 """
-
 from django.test import SimpleTestCase
-
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-
     """Test the calc module."""
-
     def test_add_numbers(self):
-        
         """Test adding numbers together"""
-
         result = calc.add(3, 4)
-
         self.assertEqual(result, 7)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        res = calc.subtract(6, 3)
+        self.assertEqual(res, 3)
